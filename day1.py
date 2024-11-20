c97bafc34bfa0efc4399700002d6a3a92b591193
clearpill <155787430+clearpill@users.noreply.github.com>
2024-11-19 21:31:52 -0500

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,22 @@
+for someone who has eidetic memory
+i sure do think and hyperfixiate on memories and theories on memories .
+
+memories are extremely complex per person, some people have good short term, bad long term, vice versa .
+
+trauma affects memories also, giving you hightened memory. like PTSD, flashbacks, but also PTSD can damage memory .
+
+when the brain processes a memory or any information it goes through a sorting process of "is this important"
+so if you typically dont care (genuinley) it will be YEETED by sorting . 
+
+but when you DO care, atleast about knowing it later . is there a time frame before your brain yeets it? or does it deem it not important anymore and filter it because you havent thought about it in awhile?
+
+this is something that kindof scares me about memories . what if i dont think about my favorite memories for awhile and they're gone? 
+
+loving to dissociate and having eidetic memory i feel like has maybe helped me keep so many long term memories, when i want to leave, I LEAVE, I GO there, remembering every detail, the smell, what was on tv, the spec of lint on the floor .
+
+but whenever i stop dissociating due to therapy will i lose all these memories? will i lose all these places and movies of the people i love and places and moments i cherish so much?
+
+this is something i really like about myself, especially because im mostly alone, introverted and hard to reach . THIS is how i spend time with people .
+
+like yeah, journaling is great for this, but looking into peoples eyes crystal clear once again while laying in a dark cold room alone . or going back to smelling the grass or pavement outside at a place i adore . 
+remebering the moss and smell of the tree that was cut down that had my tire swing growing up, doesnt really compare . 

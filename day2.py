5ba353462bacc78a31a1d5c98eabd08b4caf18ee
clearpill <155787430+clearpill@users.noreply.github.com>
2024-11-25 06:24:52 -0500

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,40 @@
+ive been thinking alot about long term plans lately. like .. long long .
+my 1-5 year plans are always so unfulfilling to obtain . 
+
+i spend most of the time carving out every single second of detail and step that its all so predictable . 
+im greatful for how much work ive put into being driven and dedicated into myself over the course of my life, having strong boundraies and having the discipline .
+
+but maybe its just me having an expectiation of how it (quote) " should feel " .
+
+once i hit the goal im just looking at the next, more and more, like ok, and its never fulfilling . just seeking more .
+
+once the to do or goal is written, its seen as a minor hurdle . 
+once its met its never really acknowledged again .
+
+i dont really celebrate or acknowledge my achievements .
+
+  if anything it just solitifies my knowing of i can complete harder and harder challenges . 
+theres no emotion tied to it . 
+  its just seen as another piece of data in my mental analytic sheet of being able to problem sovle and grow .
+
+it reminds me alot of how i feel about tetris - im level 3026 at the moment
+
+once i complete the hardest challendges or levels and having 10 pieces left over, or over excelling it just shows me im operating or have tasks that are simply not matching my level mentally .
+that i can go even harder . 
+
+  but the thing is i dont even enjoy when something is TOO hard . its over simulating and adds even more cortisol which can jenga your whole routine .
+
+I feel like im operating just below the sweet spot of equally challanging and easy to obtain . 
+
+but i dont belive in just straight skipping other improvements, esp just so i get the challenging (quote) " reward " )  i feel like everything should grow exqually and balanced . 
+
+maybe im just very close to leveling everything in my life to the point where it feels fulfilling, im proud of myself but its never enough , i KNOW i can do better than yesterday . 
+
+maybe i just have to let go my expectations of how this ( " should " ) feel and stop over analyzing or spending too much time confused about it . and just accept it for what it is . 
+
+when its put in perscpective of how/where ( " year ago me " ) would feel about now me, it generally helps and in that moment i truly do feel proud or shocked . 
+
+maybe this is just all from truama , but in a way it does keep me in a never ending cycle of self-improvment, which isnt nessisarily bad .
+
+  maybe its just all perspective .
+
